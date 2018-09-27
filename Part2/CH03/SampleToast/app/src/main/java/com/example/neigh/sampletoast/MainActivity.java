606ef7a0ac17b41331e8101571bb3f18730f99d3
박필홍 <feelongpark>
2018-09-27 20:11:43 +0900

    p366

--- Part2/CH03/SampleToast/app/src/main/java/com/example/neigh/sampletoast/MainActivity.java
+++ Part2/CH03/SampleToast/app/src/main/java/com/example/neigh/sampletoast/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.neigh.sampletoast;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,28 +21,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = (EditText)findViewById(R.id.editText);
-        editText2 = (EditText)findViewById(R.id.editText2);
+        editText = (EditText) findViewById(R.id.editText);
+        editText2 = (EditText) findViewById(R.id.editText2);
     }
 
-    public void onButton1Clicked(View v){
-        try{
+    public void onButton1Clicked(View v) {
+        try {
             Toast toastView = Toast.makeText(this, "위치가 바뀐 토스트 메세지입니다.",
                     Toast.LENGTH_LONG);
             int xOffset = Integer.parseInt(editText.getText().toString());
             int yOffset = Integer.parseInt(editText2.getText().toString());
 
-            toastView.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.BOTTOM, xOffset, yOffset);
+            toastView.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM, xOffset, yOffset);
             toastView.show();
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             e.printStackTrace();
         }
     }
 
-    public void onButton2Clicked(View v){
+    public void onButton2Clicked(View v) {
         LayoutInflater inflater = getLayoutInflater();
 
-        View layout = inflater.inflate(R.layout.toastborder,
-                (ViewGroup)findViewById(R.id.))
+        View layout = inflater.inflate(R.layout.toastborder, (ViewGroup) findViewById(R.id.toast_layout_root));
+
+        TextView text = (TextView) layout.findViewById(R.id.text);
+        Toast toast = new Toast(this);
+        text.setText("모양 바꾼 토스트");
+        toast.setGravity(Gravity.CENTER, 0, -100);
+        toast.setDuration(Toast.LENGTH_SHORT);
+
+        toast.setView(layout);
+        toast.show();
+    }
+
+    public void onButton3Clicked(View v) {
+        Snackbar.make(v, "스낵바입니다", Snackbar.LENGTH_LONG).show();
     }
 }
